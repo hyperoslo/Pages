7ff3eb86effac476afe889dce3fd7fd09a9b9364
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-08-18 09:02:30 +0200

    move Array to separate file

--- Source/PagesController.swift
+++ Source/PagesController.swift
@@ -223,22 +223,3 @@ extension PagesController {
       multiplier: 1, constant: Dimensions.bottomLineHeight))
   }
 }
-
-extension Array {
-
-  func at(index: Int?) -> T? {
-    if let index = index where index >= 0 && index < endIndex {
-      return self[index]
-    } else {
-      return nil
-    }
-  }
-}
-
-func nextIndex(x: Int?) -> Int? {
-  return x.map { $0 + 1 }
-}
-
-func prevIndex(x: Int?) -> Int? {
-  return x.map { $0 - 1 }
-}
